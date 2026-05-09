9b75f920f3a114bacf58b71d8b4f77950f292d5c
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-05-09 16:28:47 -0600

    implementación según video

--- Matrices/Matrices.cpp
+++ Matrices/Matrices.cpp
@@ -1,19 +1,23 @@
-// Matrices.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include "Matrix.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using std::cout;
+using std::endl;
+
+int main() {
+    Matrix<int> matriz(10, 15);
+	for (int i = 0; i < matriz.getRows(); i++) {
+        for (int j = 0; j < matriz.getColumns(); j++) {
+            matriz.setValue(i, j, i + j);
+        }
+    }
+
+    for (int i = 0; i < matriz.getRows(); i++) {
+        for (int j = 0; j < matriz.getColumns(); j++) {
+            cout << matriz.getValue(i, j) << "\t";
+        }
+        cout << endl;
+	}
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
---
+++ Matrices/Matrix.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <stdexcept>
+
+using std::runtime_error;
+
+template <typename E>
+class Matrix{
+private:
+	int rows, columns;
+	E** matrix;
+
+public:
+	Matrix(int rows, int columns) {
+		if (rows <= 0 || columns <= 0)
+			throw runtime_error("Filas y columnas deben ser positivas ");
+		this->rows = rows;
+		this->columns = columns;
+		matrix = new E* [rows];
+		for (int i = 0; i < rows; i++) {
+			matrix[i] = new E[columns];
+		}
+	}
+	~Matrix() {
+		for (int i = 0; i < rows; i++) {
+			delete[] matrix[i];
+		}
+		delete[] matrix;
+	}
+
+	E getValue(int row, int column) {
+		if (row < 0 || row >= rows)
+			throw runtime_error("fila invalida");
+		if (column < 0 || column >= columns)
+			throw runtime_error("columna invalida");
+		return matrix[row][column];
+	}
+
+	E setValue(int row, int column, E value) {
+		if (row < 0 || row >= rows)
+			throw runtime_error("fila invalida");
+		if (column < 0 || column >= columns)
+			throw runtime_error("columna invalida");
+		matrix[row][column] = value;
+		return value;
+	}
+
+	int getRows() {
+		return rows;
+	}
+
+	int getColumns() {
+		return columns;
+	}
+};
+
