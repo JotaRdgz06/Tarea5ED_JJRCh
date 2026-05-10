b65735cb19f8d4ed3d2d7e088fdb8426e27e17e4
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-05-10 14:21:04 -0600

    inicio programa principal

--- Matrices/Matrices.cpp
+++ Matrices/Matrices.cpp
@@ -1,38 +1,58 @@
 #include <iostream>
+#include <stdexcept>
+#include <string>
 #include "Matrix.h"
 
 using std::cout;
 using std::endl;
+using std::runtime_error;
+using std::string;
+using std::getline;
+using std::stoi;
+using std::cin;
 
 int main() {
-    Matrix<int>* matriz = new Matrix<int>(2, 5);
+	int filas, columnas;
+    cout << "Ingrese la cantidad de filas: ";
+    cin >> filas;
+
+	cout << "Ingrese la cantidad de columnas: ";
+	cin >> columnas;
+
+    Matrix<int>* matriz = new Matrix<int>(filas,columnas);
 
     for (int i = 0; i < matriz->getRows(); i++) {
         for (int j = 0; j < matriz->getColumns(); j++) {
-            matriz->setValue(i, j, i + j);
+            matriz->setValue(i, j, 0);
         }
     }
 
-    matriz->print();
-
-    cout << endl;
-
-    matriz->transpose();
-
-    matriz->print();
-
-    cout << endl;
-
-    matriz->removeRow(0);
-
-    matriz->print();
-
-	cout << endl;
-
-	matriz->removeColumn(0);
-
-	matriz->print();
+	int opcion;
+	do {
+		menuOpciones();
+		cout << "Ingrese su opcion: ";
+		cin >> opcion;
+	} while (opcion != 13);
 
     delete matriz;
     return 0;
+}
+
+string menuOpciones() {
+	cout << "Elija una de las opciones: " << endl;
+    cout << endl;
+	cout << "1. Mostrar el contendio de la matriz" << endl;
+	cout << "2. Obtener valor" << endl;
+	cout << "3. Establecer valor" << endl;
+	cout << "4. Obtener fila" << endl;
+    cout << "5. Obtener columna" << endl;
+	cout << "6. Establecer todas" << endl;
+	cout << "7. Transponer matriz" << endl;
+	cout << "8. Añadir fila" << endl;
+	cout << "9. Añadir columna" << endl;
+	cout << "10. Eliminar fila" << endl;
+	cout << "11. Eliminar columna" << endl;
+	cout << "12. Llenar aleatorio" << endl;
+	cout << "13. Salir" << endl;
+	cout << endl;
 }
